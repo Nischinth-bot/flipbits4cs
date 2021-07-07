d5726a7fab90d5335c03fab92a409f429148f4c2
Nischinth Murari <murarins@appstate.edu>
2021-07-07 18:38:46 -0400

    Router infra added

--- src/main.js
+++ src/main.js
@@ -1,24 +1,31 @@
 import { createApp } from 'vue';
 import { createStore } from 'vuex';
+import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
 import BaseCard from './components/ui/BaseCard.vue';
 import BaseButton from './components/ui/BaseButton.vue';
 import BaseBadge from './components/ui/BaseBadge.vue';
 import BaseSpinner from './components/ui/BaseSpinner.vue';
 import BaseDialog from './components/ui/BaseDialog.vue';
-import KProgress from 'k-progress';
-import obj from './store/index.js';
+import store__ from './store/index.js';
 
-const app = createApp(App)
-const store = createStore(obj);
+const app = createApp(App);
+const store = createStore(store__);
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/home' },
+    { path: '/home', component: App }
+  ]
+});
 
 app.component('base-card', BaseCard);
 app.component('base-button', BaseButton);
 app.component('base-badge', BaseBadge);
 app.component('base-spinner', BaseSpinner);
 app.component('base-dialog', BaseDialog);
-app.component('k-progress', KProgress);
 
 app.use(store);
+app.use(router);
 
 app.mount('#app');
